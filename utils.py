6e9fe1432589520da1fde205a0e429910a3f92f3
Marcus Xavier <marcus.xavier@appmax.com.br>
2022-11-18 17:50:20 -0300

    create utils file

---
+++ utils.py
@@ -0,0 +1,33 @@
+from time import sleep
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+def scrape_jobs(driver: WebDriver) -> list[WebElement]:
+    sleep(5)
+    scroll_until_end(driver)
+    return get_jobs(driver)
+
+
+def scroll_until_end(driver: WebDriver):
+    scroll_page(driver, 4)
+    scroll_page(driver, 3)
+    scroll_page(driver, 2)
+    scroll_page(driver, 1)
+
+
+def get_jobs(driver: WebDriver):
+    return driver.find_elements(By.CSS_SELECTOR, '.scaffold-layout__list-container>li>div>div.job-card-container')
+
+
+def scroll_page(driver: WebDriver, part_of_page: int):
+    if part_of_page != 0:
+        driver.execute_script(f"document.querySelector('.jobs-search-results-list').scroll(0, document.querySelector('.jobs-search-results-list').scrollHeight / {part_of_page})")
+        sleep(2)
+
+
+def create_url(url: str, next_page: int) -> str:
+    if 'start=' in url:
+        return url + f'&start={next_page * 25 + 1}'
+    return re.sub('start=[0-9][0-9]?[0-9]?', f'start={next_page * 25 + 1}', url)
